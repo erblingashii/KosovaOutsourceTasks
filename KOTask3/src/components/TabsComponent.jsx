0787eb609d08e0eacf0c212afa2bbeafd21e8a9b
erblingashii <ewinew01@gmail.com>
2024-07-30 23:04:29 +0200

    hamburger menu removed

--- KOTask3/src/components/TabsComponent.jsx
+++ KOTask3/src/components/TabsComponent.jsx
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
 import './TabsComponent.css';
 
 const TabsComponent = ({ tabs, onSelect }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(prevState => !prevState);
-  };
-
   return (
     <div className="tabs-container">
-      <button className="menu-button" onClick={toggleMenu}>
-        ☰
-      </button>
       <Tabs onSelect={onSelect}>
-        <TabList className={`react-tabs__tab-list ${isMenuOpen ? 'open' : ''}`}>
+        <TabList className="react-tabs__tab-list">
           {tabs.map((tabName, index) => (
             <Tab key={index} className="react-tabs__tab">
               {tabName}
             </Tab>
           ))}
         </TabList>
-        {tabs.map((tabName, index) => (
-          <TabPanel key={index}>
-          </TabPanel>
-        ))}
       </Tabs>
     </div>
   );
